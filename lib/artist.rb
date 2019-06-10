1b92a5f7b42622fae7e4abf6bec926920d1b8d11
kevin lang <langkevina@gmail.com>
2019-06-10 13:25:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,6 @@ require 'pry'
 class Artist
   attr_accessor :name
   @@all = []
-  @@song_count = 0
   def initialize(name)
     @name = name
   end
@@ -14,20 +13,15 @@ class Artist
  def add_song(song)
    song.artist = self
    @@all << song
-   @@song_count += 1
-    binding.pry
  end
  #
  def add_song_by_name(name)
    song = Song.new(name)
    song.artist = self
-   @@song_count += 1
    @@all << song
-    binding.pry
  end
 
  def self.song_count
-   binding.pry
    @@all.length
  end
 end
