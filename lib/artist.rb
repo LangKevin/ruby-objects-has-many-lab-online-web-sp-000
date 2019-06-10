6e7a38f9935b9efc4170d5704a12d902fc62b1d0
kevin lang <langkevina@gmail.com>
2019-06-10 12:25:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,10 +9,10 @@ class Artist
    @songs << song
  end
  #
- # def add_song_by_name(name)
- #   song = Song.new(name)
- #   @songs << song
- #   song.artist = self
- # end
+ def add_song_by_name(name)
+   song = Song.new(name)
+   @songs << song
+   song.artist = self
+ end
 
 end
