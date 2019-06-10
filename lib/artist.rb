e9bad5d89f4af44c3af3bceead54e6afc0dde315
kevin lang <langkevina@gmail.com>
2019-06-10 12:24:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
 class Artist
  # attr_accessor :name
- # def initialize(name)
- #   @name = name
- #   @songs = []
- # end 
+ def initialize(name)
+   @name = name
+   @songs = []
+ end 
  #
  # def add_song(song)
  #   @songs << song
