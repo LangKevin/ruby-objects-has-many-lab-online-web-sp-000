faea98aea9c18f429590bd107e6a91886a4ea571
kevin lang <langkevina@gmail.com>
2019-06-10 12:23:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,18 @@
 class Artist
- attr_accessor :name
- def initialize(name)
-   @name = name
-   @songs = []
- end 
-
- def add_song(song)
-   @songs << song
- end
-
- def add_song_by_name(name)
-   song = Song.new(name)
-   @songs << song
-   song.artist = self
- end
+ # attr_accessor :name
+ # def initialize(name)
+ #   @name = name
+ #   @songs = []
+ # end 
+ # 
+ # def add_song(song)
+ #   @songs << song
+ # end
+ # 
+ # def add_song_by_name(name)
+ #   song = Song.new(name)
+ #   @songs << song
+ #   song.artist = self
+ # end
  
 end
