ac4c4ff2b11ab67389b2a2688524d3766936d970
kevin lang <langkevina@gmail.com>
2019-06-10 12:25:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,9 +5,9 @@ class Artist
     @song = []
   end
  #
- # def add_song(song)
- #   @songs << song
- # end
+ def add_song(song)
+   @songs << song
+ end
  #
  # def add_song_by_name(name)
  #   song = Song.new(name)
